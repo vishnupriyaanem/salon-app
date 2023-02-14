a6929939a6e9ecbe5a1cd5b834965533635e4374
harinder singh <harinder.singh@serrala.com>
2023-02-14 09:23:01 +0530

    frontend project

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Constants from 'expo-constants';
 export default function App() {
+  useEffect(()=>{
+console.log(Constants.statusBarHeight)
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start workin on your app!</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop:Constants.statusBarHeight
   },
 });
